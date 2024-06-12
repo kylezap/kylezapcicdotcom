3bb4b2319e99cbe7d92f2a17fc1601d1f0d5c43c
kylezap <kzapcic+github@gmail.com>
2024-06-12 15:08:45 -0400

    fix width of second para

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -14,7 +14,7 @@ export default function About() {
           About
         </Typography>
         <Grid container spacing={2}>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <Typography variant="body1" component="div">
               {
                 "I am a passionate and curious lifelong learner with a rich and varied career spanning multiple industries. As a fullstack developer, my deep-seated love for problem-solving drives me to continuously seek new knowledge and innovative solutions in the tech world. Throughout my professional journey, I have honed my skills in relationship building and client engagement, consistently fostering strong connections and delivering exceptional results. My proactive nature and hands-on approach have established me as a doer, always ready to tackle challenges head-on and drive projects to successful completion. This blend of enthusiasm, technical expertise, curiosity, and experience enables me to thrive in dynamic environments and make meaningful contributions wherever I go. "
@@ -23,12 +23,12 @@ export default function About() {
             </Typography>
             <br />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <img src={catalina} alt="catalina" className="about" />
           </Grid>
         </Grid>
         
-          <Typography variant="body1" component="div" sx={{ m: 4}} maxWidth='xl'>
+          <Typography variant="body1" component="div" sx={{ my: 4}}>
             {
               "React, Node.js, Express, and MongoDB are just a few of the tools in my developer toolkit. I am also well-versed utilizing things other tools like GraphQL, REST API's, and Postgres, and I am always eager to learn new technologies and frameworks. My passion for creating modern, responsive, and impactful web applications is matched only by my dedication to building user-friendly interfaces that enhance the user experience. I am excited to continue growing as a developer and to leverage my skills to create innovative solutions that make a difference in the world. When I am not coding, you can find me surfing, hiking, or exploring the great outdoors. I am also an avid reader, a music lover, and a lifelong learner who is always seeking new adventures and experiences. I am excited to connect with like-minded individuals and collaborate on exciting projects that push the boundaries of what is possible in the tech world. Let's build something amazing together!"
             }
